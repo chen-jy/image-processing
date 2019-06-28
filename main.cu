3e92d88e02619c46afc67e1404e94808bbb0db6a
Yijia Chen <jyc.chen@mail.utoronto.ca>
2019-06-28 14:33:20 -0400

    Add filters

--- main.cu
+++ main.cu
@@ -26,6 +26,39 @@ void destroy_png(my_png *png) {
 	free(png);
 }
 
+
+struct filter {
+	int dim;
+	int *matrix;
+};
+
+struct filter_dp {
+	int dim;
+	double *matrix;
+};
+
+int _id3_m[] = { 0, 0, 0, 0, 1, 0, 0, 0, 0 };
+filter _id3 = { 3, _id3_m };
+
+int _edge3_m[] = { -1, -1, -1, -1,  8, -1, -1, -1, -1 };
+filter _edge3 = { 3, _edge3_m };
+
+int _sharp3_m[] = { 0, -1,  0, -1,  5, -1, 0, -1,  0 };
+filter _sharp3 = { 3, _sharp3_m };
+
+int _lapgaus9_m[] = { 0, 1, 1, 2, 2, 2, 1, 1, 0, 1, 2, 4, 5, 5, 5, 4, 2, 1,
+	1, 4, 5, 3, 0, 3, 5, 4, 1, 2, 5, 3, -12, -24, -12, 3, 5, 2,
+	2, 5, 0, -24, -40, -24, 0, 5, 2, 2, 5, 3, -12, -24, -12, 3, 5, 2,
+	1, 4, 5, 3, 0, 3, 5, 4, 1, 1, 2, 4, 5, 5, 5, 4, 2, 1, 0, 1, 1, 2, 2, 2, 1, 1, 0 };
+filter _lapgaus9 = { 9, _lapgaus9_m };
+
+double _blur5_m[] = { 1, 4, 6, 4, 1, 4, 16, 24, 16, 4, 6, 24, 36, 24, 6,
+	4, 16, 24, 16, 4, 1, 4, 6, 4, 1 };
+filter_dp dp_filter = { 5, _blur5_m };
+
+filter *filters[4] = { &_id3, &_edge3, &_sharp3, &_lapgaus9 };
+
+
 // PNG reading and writing adapted from https://gist.github.com/niw/5963798
 my_png *read_png(string filename) {
 	my_png *myPNG = (my_png*)Malloc(sizeof(my_png));
@@ -106,6 +139,12 @@ int write_png(string filename, my_png *myPNG) {
 	return 0;
 }
 
+
+void init() {
+	for (int i = 0; i < 25; i++)
+		dp_filter.matrix[i] *= (1.0 / 256.0);
+}
+
 int main(int argc, char *argv[]) {
 	string input_filename, cpu_filename, gpu_filename;
 	if (argc < 3) {
@@ -128,6 +167,8 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 	}
+
+	init();
 
 	my_png *png = read_png(input_filename);
 	write_png(cpu_filename, png);
